bfa992ecfeac5296f286d1434a70dad15cb64521
buraktopcu12@hotmail.com <buraktopcu12@hotmail.com>
2021-03-21 17:10:04 +0300

    added env port for testing

--- account-service/src/main/java/com/ecommerce/accountservice/controller/AccountController.java
+++ account-service/src/main/java/com/ecommerce/accountservice/controller/AccountController.java
@@ -1,5 +1,7 @@
 package com.ecommerce.accountservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/account/")
 public class AccountController {
 
+    private final Environment env;
+
+    @Autowired
+    public AccountController(Environment env) {
+        this.env = env;
+    }
+
     @GetMapping("/hello")
     public String hello(){
-        return "Hello from account controller";
+        return "Hello from account controller" + env.getProperty("local.server.port");
     }
 }
