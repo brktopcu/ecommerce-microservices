6a79104e08875888ab3860458d236f03c7d261ee
buraktopcu12@hotmail.com <buraktopcu12@hotmail.com>
2021-05-02 21:40:50 +0300

    account cleanup

--- account-service/src/main/java/com/ecommerce/accountservice/service/MapValidationErrorService.java
+++
@@ -1,9 +0,0 @@
-package com.ecommerce.accountservice.service;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-
-public interface MapValidationErrorService {
-
-    ResponseEntity<?> mapValidationService(BindingResult result);
-}
